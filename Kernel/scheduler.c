adcbc670b67c63ad377f51647f2016c8b504a84d
Matias <mdaneri98@gmail.com>
2023-05-18 19:03:28 -0300

    Se avanzó con el scheduler.

--- Kernel/scheduler.c
+++ Kernel/scheduler.c
@@ -4,8 +4,8 @@
 
 typedef struct ProcessControlBlockCDT {
     char* name;
-    int id;
-    int priority;
+    unsigned int id;
+    unsigned int priority;
     char foreground;
     ProcessState state;
     uint64_t stack;
@@ -13,6 +13,7 @@ typedef struct ProcessControlBlockCDT {
 
 typedef struct node {
     struct node *next;
+    struct node *previous;
     ProcessControlBlockCDT pcbEntry;
 } node_t;
 
@@ -50,18 +51,21 @@ void init() {
 // ----------- Implementación Round-Robin sin prioridad ----------------
 
 void add(ProcessControlBlockCDT newEntry) {
+    node_t *previous = NULL;
     node_t *current = &linkedList.head;
     while (current->next != NULL) {
-        current = current->next;
+            previous = current;
+            current = current->next;
     }
 
-    node_t *newNode = &current + sizeof(ProcessControlBlockCDT);
+    node_t *newNode = current + sizeof(ProcessControlBlockCDT);
 
     newNode->pcbEntry.id = newEntry.id;
     newNode->pcbEntry.name = newEntry.name;
     newNode->pcbEntry.priority = newEntry.priority;
     newNode->pcbEntry.stack = newEntry.stack;
     newNode->pcbEntry.state = newEntry.state;
+    newNode->previous = current;
 
     current->next = newNode;
     newNode->next = NULL;
@@ -77,7 +81,7 @@ ProcessControlBlockCDT next() {
     node_t *current = &linkedList.current;
 
     // Si estamos en el último nodo, nos movemos al inicio.
-    if (current->next == NULL) {
+    if (current->pcbEntry.state != READY && current->next == NULL) {
         current = &linkedList.head;
     }
 
@@ -97,6 +101,32 @@ ProcessControlBlockCDT next() {
 }
 
 
+ProcessControlBlockCDT remove(unsigned int id) {
+    /* Si no lo encuentra => Loop infinito */
+
+    node_t *current = &linkedList.current;
+    if (current->pcbEntry.id != id && current->next == NULL) {
+        current = &linkedList.head;
+    }
+
+    while (current->pcbEntry.id != id && current->next != NULL) {
+        current = current->next;
+
+        if (current->next == NULL) {
+            if (current->pcbEntry.id != id) {
+                current = &linkedList.head;
+            }
+        }
+    }
+
+    if (current->pcbEntry.id == id) {
+        current->previous = current->next;
+    }
+
+
+}
+
+
 
 void scheduler() {
     /* La primera vez que se ejecuta el scheduler, debe iniciar el proceso init */
@@ -104,6 +134,19 @@ void scheduler() {
         init();
         firstTime = 0;
     }
+
+    /* Si fue llamado manualmente, mediante int 0x20, tenemos que
+        revisar el estado de este proceso */
+    if (linkedList.current.pcbEntry.state == EXITED) {
+        node_t aux = linkedList.current;
+        
+        //Movemos el puntero al siguiente en ejecutar.
+        ProcessControlBlockCDT _ = next(); 
+        
+        //Eliminamos el proceso actual (que está en estado EXITED) de la lista.
+        remove(aux.pcbEntry.id);
+    }
+
 
     ProcessControlBlockCDT pcbEntry = next();
     
