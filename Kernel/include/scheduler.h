60a14041ee261dd461115b5e979e5e894c9bcb51
Matias <mdaneri98@gmail.com>
2023-05-18 18:11:23 -0300

    Se comenzó a implementar el algoritmo de Round-Robin sin prioridad.

--- Kernel/include/scheduler.h
+++ Kernel/include/scheduler.h
@@ -3,7 +3,7 @@
 #define PCB_TABLE 0x0000000000010000
 
 // States for processes.
-typedef enum ProcessState { READY, RUNNING, BLOCKED } ProcessState;
+typedef enum ProcessState { READY, RUNNING, BLOCKED, TERMINATED } ProcessState;
 
 
 typedef struct ProcessControlBlockCDT *ProcessControlBlockADT;
