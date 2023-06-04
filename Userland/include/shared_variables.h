9c72cbf6810ebc39ff875b3a2b02f2913c82380b
Matias <mdaneri98@gmail.com>
2023-06-04 16:16:25 -0300

    fixed

---
+++ Userland/include/shared_variables.h
@@ -0,0 +1,11 @@
+#ifndef SHARED_VARIABLES_H
+#define SHARED_VARIABLES_H
+
+#include <filosopher.h>
+
+extern sem_t* s[]; 
+extern phylState state[]; 
+extern char ansiArt[]; 
+extern sem_t* mutexSem;
+
+#endif
